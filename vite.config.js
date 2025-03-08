79576f9a3247e408014df45995c92ce9f8d7066c
Gabriel Mazzuchello Dal Molin <94196035+GabrielMazzuchello@users.noreply.github.com>
2025-03-08 09:40:04 -0300

    configurações de deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/edmc/", // Deve corresponder EXATAMENTE ao nome do repositório
+  base: "/edmc", // Deve corresponder EXATAMENTE ao nome do repositório
   chunkSizeWarningLimit: 1500,
   build: {
     outDir: "dist",
